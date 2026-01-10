e92fd15775c443de5ae68fa9a13d451736ebded0
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-01-11 03:37:31 +0500

    alpha v0.0.2.1

--- DOM.js
+++ DOM.js
@@ -5,12 +5,14 @@ var DOM =(function(){
     }
     //Публичные функции
     return {
-        elDisabled: function(id){
-            document.getElementById(id).disabled = "disabled";
+        elDisabled: function(id, bool){
+            document.getElementById(id).disabled = bool;
+            // const el = document.getElementById(id);
+            // bool ? el.disabled = "disabled" : el.removeAttribute("disabled");
         }
     }
 })();
 // function elDisabled(id){
 //     document.getElementById(id).disabled = "disabled";
 // }
-//alpha v0.0.2
+//alpha v0.0.2.1
