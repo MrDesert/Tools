a9a90d43740cb1fd4c8ba347e03aafa8d46ee5a1
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-01-30 16:11:27 +0500

    alpha v0.3.6.5

--- math.js
+++ math.js
@@ -1,3 +1,14 @@
 function mathTriangularNumber(N){
     return N * (N+1)/2;
 }
+
+function mathTriangularNumberInverse(result) {
+    // N² + N - 2*result = 0
+    // Дискриминант: D = 1 + 8*result
+    // N = (-1 + √D) / 2 (только положительный корень)
+    
+    const D = 1 + 8 * result;
+    const N = (-1 + Math.sqrt(D)) / 2;
+    
+    return N;
+}
