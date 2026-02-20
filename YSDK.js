9fbc2378c71452e6761f77716612ce6aeb5d8e3c
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-02-20 21:18:20 +0500

    alpha v0.3.7.1

---
+++ YSDK.js
@@ -0,0 +1,57 @@
+let resurses = false;
+let sdkLoad = false;
+
+function useSDK(bool){
+if(bool){
+    const script = document.createElement('script');
+        script.src = "https://yandex.ru/games/sdk/v2";
+        script.async = true;
+    document.head.appendChild(script);
+    script.onload = function(){
+        myLog?.("script.onload");
+        YaGames.init().then(ysdk => {
+            const langSDK = ysdk.environment?.i18n?.lang; //Запрашиваем язык у YSDK
+            if(langSDK == "ru"){document.getElementById("loading").textContent = "Загрузка"}
+            else{document.getElementById("loading").textContent = "Loading"};
+            myLog?.("язык - " + ysdk.environment?.i18n?.lang);
+            changeLang(langSDK);
+            document.documentElement.lang = langSDK;
+            window.ysdk = ysdk;
+            if(window.ysdk){
+                sdkLoad = true;
+                myLog?.("SDK загружен - " + sdkLoad);
+            }
+        })
+        .then(()=>{
+            return new Promise((resolve)=>{
+                const chekcInterval = setInterval(()=>{
+                    if (sdkLoad && resurses){
+                        myLog?.("Загруженны ресурсы и SDK");
+                        ysdk.features?.GameplayAPI?.start?.();
+                        clearInterval(chekcInterval);
+                        resolve();
+                    }
+                    else if(sdkLoad){myLog?.("Ожидание ресурсов - " + resurses)}
+                    else if (resurses){myLog?.("Ожидание SDK - " + sdkLoad)}
+                    else{myLog?.("Ожидание ресурсов и CDK");}
+                }, 400);
+            })
+        })
+        .then(()=>{                 
+            ysdk.features.LoadingAPI.ready();
+        })
+        .then(()=>{
+            setTimeout(function(){
+                startGame();
+            }, 30);
+        })
+        .catch(console.error);
+    };
+    window.addEventListener('load', function(){
+        resurses = true;
+        myLog?.("Ресурсы загруженны - " + resurses);
+    }) 
+} else {
+    setTimeout(function(){ sdkLoad =true; resurses=true; startGame();}, 1000);
+}
+}
